b5d39776cfcdcce2dc65be243084e240db2010ee
marrionluaka <davedsmithjr@gmail.com>
2016-02-04 11:06:01 -0800

    Organic Launch Prototype
    Phase 2

---
+++ Organic_Launch/Organic_Launch/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Organic_Launch.Controllers
+{
+    public class HomeController : Controller
+    {
+        //-------------------------------------Home
+        private OrganicLaunchEntities db = new OrganicLaunchEntities();
+        public ActionResult Index()
+        {
+            
+            return View(db.Farms.ToList());
+        }
+
+    }
+}
---
+++ Organic_Launch/Organic_Launch/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Organic_Launch.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Single()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
